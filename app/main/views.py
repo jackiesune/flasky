cd7e64752dc1ea77cceaeaffd0bb96ebf103ddc5
jack.s <my_email@exp.co>
2018-12-26 22:25:44 +0800

    change views

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from . import main 
 from .. import db
 from ..models import User
-from ..emaid import send_email
+from ..email import send_email
 from .forms import NameForm
 
 @main.route('/',methods=['GET','POST'])
