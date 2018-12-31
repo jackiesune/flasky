da53502c18f28d942bb3130d3d6bc1a00eaed990
jack.s <my_email@exp.co>
2018-12-31 11:54:41 +0800

    add perssion

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from  werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+
+
 
 @login_manager.user_loader
 def loader_user(user_id):
@@ -16,11 +20,55 @@ class Role(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
     users=db.relationship("User",backref='role',lazy='dynamic')
+    default=db.Column(db.Boolean,default=False,index=True)
+    permissions=db.Column(db.Integer)
 
     
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    @staticmethod
+    def insert_roles():
+        roles={
+            'User':(Permission.FOLLOW|
+                    Permission.COMMENT|
+                    Permission.WRITE_ARTICLES,True),
+            'Moderator':(Permission.FOLLOW|
+                         Permission.COMMENT|
+                         Permission.WRITE_ARTICLES|
+                         Permission.MODERATE_COMMENTS,False),
+            'Administrator':(0Xff,False)
+        }
+        for r in roles:
+            role=Role.query.filter_by(name=r).first()
+            if role is None:
+                role=Role(name=role)
+                role.default=roles[r][1]
+                role.perssions=roles[r][0]
+                db.session.add(role)
+            db.session.commit()
+
+
+
+
+class Permission:
+    FOLLOW=0X01
+    COMMENT=0X02
+    WRITE_ARTICLES=0X04
+    MODERATE_COMMENTS=0X08
+    ADMINISTER=0X80
+
+
+
+
+
+
+
+
+
+
+
 
 class User(UserMixin,db.Model):
     __tablename__='users'
@@ -29,8 +77,26 @@ class User(UserMixin,db.Model):
     password_hash=db.Column(db.String(128))
     email=db.Column(db.String(64),unique=True,index=True)
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    confirmed=db.Column(db.Boolean,default=False)
 
 
+    def generat_cofirmation_token(self,expiration=2600):
+        s=Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+
+    def cofirm(self,token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.dumps(token)
+        except:return  False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirmed=True
+        db.session.add(self)
+        db.session.commit()
+        return True
+        
+        
 
 
 
